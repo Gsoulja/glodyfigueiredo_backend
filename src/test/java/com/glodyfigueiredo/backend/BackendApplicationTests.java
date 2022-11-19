c355e877eadf5a38dfde0107cddab73a882ff665
Glody Figueiredo <glodyfigueiredo@outlook.com>
2022-11-19 10:05:26 +0000

    initial commit

---
+++ src/test/java/com/glodyfigueiredo/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glodyfigueiredo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
